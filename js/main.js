e0073287806dd36b9cd1153b775a743487ef77d2
21DTHA2_2180605768 <duynguyenhoang203@gmail.com>
2025-06-12 13:23:08 +0700

    up register

--- js/main.js
+++ js/main.js
@@ -9,3 +9,13 @@ window.addEventListener("scroll", function (e) {
         nav.classList.add("bg-transparent");
     }
 })
+changeLogin = ()=>{
+    document.getElementById("registerForm").classList.add("hidden");
+    document.getElementById("loginForm").classList.remove("hidden");
+    document.getElementById("tittle").innerHTML=  `welcome back`;
+}
+changeRegister = ()=>{
+    document.getElementById("loginForm").classList.add("hidden");
+    document.getElementById("registerForm").classList.remove("hidden");
+        document.getElementById("tittle").innerHTML=  `Register now`;
+}
